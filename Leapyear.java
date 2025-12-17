ca03290f0a1db73c1f85c3cec4de00ac6669ae2c
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-12-17 19:20:19 +0530

    Just remember 4

---
+++ Leapyear.java
@@ -0,0 +1,16 @@
+
+public class Leapyear {
+	public static void main(String[] args) {
+		// when the year is divisible by 4 then it is a leap year
+		
+		int a = 2028;
+		
+		if (a%4 == 0) {
+			System.out.println("It is an leap year");
+		}
+		else {
+			System.out.println("It is not an leap year");
+		}
+	}
+
+}
