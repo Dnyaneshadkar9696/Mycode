1d40701774e953528a00df02c9948a74a153fbdf
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-12-17 18:11:57 +0530

    The Great Number

---
+++ GreatNum.java
@@ -0,0 +1,34 @@
+//import java.util.Scanner;
+public class GreatNum {
+	public static void main(String[] args) {
+		int a = 15000;
+		int b = 900;
+		int c = 4345340;
+		int d = 0;
+		int e = 0;
+		
+		// program to find the greater number among above numbers
+		
+		// operators help you a lot while writhing code and checking the facts
+		
+		// i have to check using operators 
+		if(a>b && a>c) {
+			d = a;
+			System.out.println(a);
+		}
+		else if(b>a && b>c) {
+			d = b;
+			System.out.println(b);
+		}
+		else if(d>c) {
+			e = d;
+			System.out.println(e);
+		}
+		else {
+			e = c;
+			System.out.println(e);
+		}
+		
+	}
+
+}
