60614d47db3e90f6890f14cc095ac2fa28470a7d
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-12-20 12:50:13 +0530

    Table print from input.
    Printing the multiplication and giving table for any number provided.

---
+++ TableMulti.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class TableMulti {
+	
+	public static void main(String[] args) {
+		
+		// program to print the multiplication table in java.
+		Scanner ut = new Scanner(System.in);
+		int a =0;
+		System.out.println("Enter the number to print its table.");
+		a = ut.nextInt();
+		
+		System.out.println("The table as follows: ");
+		
+		System.out.println(a + " * 1 = "+ (a*1));
+		System.out.println(a + " * 2 = "+ (a*2));
+		System.out.println(a + " * 3 = "+ (a*3));
+		System.out.println(a + " * 4 = "+ (a*4));
+		System.out.println(a + " * 5 = "+ (a*5));
+		System.out.println(a + " * 6 = "+ (a*6));
+		System.out.println(a + " * 7 = "+ (a*7));
+		System.out.println(a + " * 8 = "+ (a*8));
+		System.out.println(a + " * 9 = "+ (a*9));
+		System.out.println(a + " * 10 = "+ (a*10));
+		
+		
+		
+		
+		
+	}
+
+}
